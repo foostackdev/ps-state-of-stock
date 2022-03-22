2024030b422ddf5334e6dac9dbd063ca7a3f5831
Max <foostackdev@gmail.com>
2022-03-22 20:12:07 +0800

    feat: display last update date time in utc

--- scrape.js
+++ scrape.js
@@ -1,4 +1,8 @@
-({
-    result: document.querySelector('.box__content').innerText === 'Oops! Something has gone wrong.\nReturn To Home Page',
-    timestamp: new Date().toLocaleDateString('en-us', { year: 'numeric', month: 'long', day: 'numeric' })
-})
+(function () {
+  return {
+    result:
+      document.querySelector('.box__content').innerText ===
+      'Oops! Something has gone wrong.\nReturn To Home Page',
+    last_update_utc: new Date().toUTCString(),
+  };
+})();
