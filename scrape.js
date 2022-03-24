5b35bb6ff004d55a8d2a1836cb8e5ac5d10f7b91
Max <foostackdev@gmail.com>
2022-03-24 20:55:30 +0800

    fix: use ECMAScript epoch timestamp

--- scrape.js
+++ scrape.js
@@ -3,6 +3,6 @@
     result:
       document.querySelector('.box__content').innerText ===
       'Oops! Something has gone wrong.\nReturn To Home Page',
-    last_update_utc: new Date().toUTCString(),
+    timestamp: new Date().getTime()
   };
 })();
