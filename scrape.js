e82f002fdaf2fdf7237011e1f1c591fbfc825487
Max <foostackdev@gmail.com>
2022-05-20 19:29:14 +0800

    fix: null check

--- scrape.js
+++ scrape.js
@@ -1,8 +1,9 @@
 (function () {
   return {
     result:
+      document.querySelector('.box__content') !== null &&
       document.querySelector('.box__content').innerText ===
-      'Oops! Something has gone wrong.\nReturn To Home Page',
+        'Oops! Something has gone wrong.\nReturn To Home Page',
     timestamp: new Date().getTime()
   };
 })();
