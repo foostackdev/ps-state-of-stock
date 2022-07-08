d51acd72c3f98dd90fd09603f5ee14ab95c82679
Max <31043116+foostackdev@users.noreply.github.com>
2022-07-08 18:50:54 +0800

    fix: proper condition checking

--- scrape.js
+++ scrape.js
@@ -1,9 +1,9 @@
 (function () {
   return {
     result:
-      document.querySelector('.box__content:not([style*="display:none"])') !== null &&
-      document.querySelector('.box__content').innerText !==
-        'Oops! Something has gone wrong.\nReturn To Home Page',
+      !(document.querySelector('.box__content:not([style*="display:none"])') !== null &&
+      document.querySelector('.box__content').innerText ===
+        'Oops! Something has gone wrong.\nReturn To Home Page'),
     timestamp: new Date().getTime()
   };
 })();
