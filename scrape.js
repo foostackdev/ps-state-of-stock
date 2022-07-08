59ef98c9a5933ceaa842624df097826131f3d63c
Max <31043116+foostackdev@users.noreply.github.com>
2022-07-08 16:27:33 +0800

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,9 +1,10 @@
 (function () {
   return {
     result:
-      document.querySelector('.box__content') !== null &&
+      document.querySelector('.list-products') !== null ||
+      (document.querySelector('.box__content') !== null &&
       document.querySelector('.box__content').innerText !==
-        'Oops! Something has gone wrong.\nReturn To Home Page',
+        'Oops! Something has gone wrong.\nReturn To Home Page'),
     timestamp: new Date().getTime()
   };
 })();
